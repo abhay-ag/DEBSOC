349be3451677640badb881ba405a2c6f18effc33
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-18 19:59:38 +0530

    Font-Family and Website Name changed | Java script added

---
+++ Contact Us/app.js
@@ -0,0 +1,7 @@
+const footer = document.querySelector('.footer')
+
+document.addEventListener('DOMContentLoaded', () =>{
+    let date = new Date;
+    let getYear = date.getFullYear();
+    footer.innerHTML = `Copyright &copy; ${getYear} DEBSOC. All rights reserved.`
+})
