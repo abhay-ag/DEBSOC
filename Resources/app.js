f8ee6a838e7a7c0b76846402fa322e55d0d9780a
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-19 16:17:14 +0530

    JavaScript added | Card Completed

--- Resources/app.js
+++ Resources/app.js
@@ -11,3 +11,55 @@ $(document).ready(() => {
         return false;
     });
 })
+
+const members = [
+    {
+        name: "John",
+        imgUrl : "https://sunrift.com/wp-content/uploads/2014/12/Blake-profile-photo-square.jpg",
+        position: "Founder and CEO",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus libero cum quaerat architecto iste delectus dolores ex blanditiis iure expedita?",
+    },{
+        name: "Milan",
+        imgUrl : "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHw%3D&w=1000&q=80",
+        position: "Lead Developer",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus libero cum quaerat architecto iste delectus dolores ex blanditiis iure expedita?",
+    },{
+        name: "Smith",
+        imgUrl : "https://profilemagazine.com/wp-content/uploads/2020/04/Ajmere-Dale-Square-thumbnail.jpg",
+        position: "Marketing Executive",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus libero cum quaerat architecto iste delectus dolores ex blanditiis iure expedita?",
+    },{
+        name: "Natasha",
+        imgUrl : "https://elireview.com/wp-content/uploads/2016/12/reed-profile-square.jpg",
+        position: "Senior Developer",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus libero cum quaerat architecto iste delectus dolores ex blanditiis iure expedita?",
+    }
+]
+
+const textContainer = document.querySelector('.people')
+
+document.addEventListener('DOMContentLoaded', () =>{
+    let text = '';
+    for(let i =0 ; i < members.length ; i ++){
+        text += `
+        <div class="main-card">
+            <div class="photo">
+                <img src="${members[i].imgUrl}">
+            </div>
+            <div class="name">
+                <h1 class="thin no-border">Hi, I'm <b>${members[i].name}</b><br><small>${members[i].position}</small></h1>
+            </div>
+            <div class="description">
+                ${members[i].description}
+            </div>
+            <div class="socials">
+                <i class="fab fa-facebook"></i>
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-linkedin"></i>
+                <i class="fab fa-instagram"></i>
+            </div>
+        </div>
+        `
+    }
+    textContainer.innerHTML = text;
+})
